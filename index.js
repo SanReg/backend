582d996af99399e5d8d32f51fe3410d248e199ba
Santosh Regmi <github@regmisantosh.com.np>
2025-05-15 17:20:23 +0545

    First Backend

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const cors = require('cors')
+
+app.use(cors({
+  origin: 'https://wrc.lol/new.html',
+  credentials: true // if you need cookies or auth headers
+}));
+
+app.use(cors()); //to allow it from everywhere
+
+app.get("/",(req,res)=>{
+    res.json({
+        name: "Santosh",
+        message: "Message from the server"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("App listening!")
+})
